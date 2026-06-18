895ccf7165c1eeace1b5b8be39afaf52f561bde7
ronaldomami902-cpu <ronaldomami902@gmail.com>
2026-06-18 12:28:09 +0300

    Create MazaConfig.java

---
+++ src/main/java/com/mazaclient/config/MazaConfig.java
@@ -0,0 +1,26 @@
+package com.mazaclient.config;
+
+public class MazaConfig {
+    public boolean espEnabled = true;
+    public boolean redstoneEnabled = true;
+    public boolean observerEnabled = true;
+    public boolean dispenserEnabled = true;
+    public boolean glassEnabled = true;
+    public boolean chestEnabled = true;
+    public boolean shulkerEnabled = true;
+    public boolean spawnerEnabled = true;
+    public boolean pistonEnabled = true;
+    public boolean chunkVoidEnabled = true;
+    public int yMin = -55;
+    public int yMax = 60;
+    public int voidThreshold = 400;
+    public float[] redstoneColor  = {1.0f, 0.1f, 0.1f, 0.9f};
+    public float[] observerColor  = {1.0f, 0.9f, 0.0f, 0.9f};
+    public float[] dispenserColor = {1.0f, 0.9f, 0.0f, 0.9f};
+    public float[] glassColor     = {0.2f, 0.5f, 1.0f, 0.7f};
+    public float[] chestColor     = {1.0f, 0.5f, 0.0f, 0.9f};
+    public float[] shulkerColor   = {1.0f, 0.5f, 0.0f, 0.9f};
+    public float[] spawnerColor   = {0.7f, 0.0f, 1.0f, 0.9f};
+    public float[] pistonColor    = {0.0f, 0.9f, 0.3f, 0.9f};
+    public float[] chunkVoidColor = {0.0f, 1.0f, 0.2f, 0.5f};
+}
