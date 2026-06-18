5d26a60947019cc99b0edbc4170671b3c498972b
ronaldomami902-cpu <ronaldomami902@gmail.com>
2026-06-18 12:31:37 +0300

    Create GameRendererMixin.java

---
+++ src/main/java/com/mazaclient/mixin/GameRendererMixin.java
@@ -0,0 +1,8 @@
+package com.mazaclient.mixin;
+
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+}
